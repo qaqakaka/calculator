5f34cdef03e9737371d51c529cab96f139b6092f
qaqakaka <haquemowmita@gmail.com>
2025-04-15 10:31:42 +0600

    Initial commit

---
+++ calculator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// Your calculator logic (no main function here)
+class Calculator
+{
+public:
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+    // Add other functions like subtract, multiply, etc.
+};
